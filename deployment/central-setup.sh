01af1d27c3a55e76818ebdebe705533f475e497b
Jingun Jung <jingun.jung@motojin.com>
2021-01-21 12:43:40 +0900

    🎷 added iot central post-script

---
+++ deployment/central-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+az extension add --name azure-iot --yes --only-show-errors
+
+resourceGroup=$1
+webAppName=$2
+subdomain=$3
+tokenId="${4:-WeDXServer}"
+
+applicationId=$(az iot central app list --query "[].{applicationId:applicationId, subdomain:subdomain}[?contains(subdomain,'${subdomain}')].applicationId" -o tsv)
+if [ -z $applicationId ]; then
+  echo "Error - Azure IoT Central Application ID does not exist : ${subdomain}"
+  exit 1
+fi
+
+searchTokenId=$(az iot central api-token list --app-id ${applicationId} --query "value[].{id:id}[?contains(id,'${tokenId}')].id" -o tsv)
+if [ ! -z $searchTokenId ]; then
+  searchTokenId=$(az iot central api-token delete --app-id ${applicationId} --tkid ${tokenId})
+  sleep 1
+  echo "The existing ID has been deleted."
+fi
+
+applicationJsonToken=$(az iot central api-token create --app-id ${applicationId} --role builder --tkid ${tokenId})
+if [[ -z $applicationJsonToken ]]; then
+  echo "Error - Creating Application Token is failed : ${tokenId}"
+  exit 1
+fi
+
+applicationToken=$(echo $applicationJsonToken | jq -r '.token' 2> /dev/null)
+
+centralSubdomain=$(az webapp config appsettings set --name ${webAppName} --resource-group ${resourceGroup} --settings WedxAppConfig__IotCentral__Subdomain=${subdomain} --query "[?name=='WedxAppConfig__IotCentral__Subdomain'].[value]" -o tsv)
+
+centralAppToken=$(az webapp config appsettings set --name ${webAppName} --resource-group ${resourceGroup} --settings WedxAppConfig__IotCentral__ApiToken="${applicationToken}" --query "[?name=='WedxAppConfig__IotCentral__ApiToken'].[value]" -o tsv)
+
+echo "Complete configuration"
