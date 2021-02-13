46b7df29bf1e0ec80a4520ba90bec805295a20da
Jingun Jung <jingun.jung@motojin.com>
2021-02-13 11:44:31 +0900

    🎁 created digital twins event routes

--- deployment/adt-setup.sh
+++ deployment/adt-setup.sh
@@ -9,10 +9,10 @@ if [ $? -ne 0 ]; then
     echo -e "azure-iot extension is now installed."
 else
     az extension update --name azure-iot &> /dev/null
-    echo -e "azure-iot extension is up to date."														  
+    echo -e "azure-iot extension is up to date."
 fi
 
-# script configuration 
+# script configuration
 RESOURCE_GROUP=$1
 ADT_NAME=$2
 ADL_SPACE_FLOOR_NAME="Space-Floor-v1.json"
@@ -21,6 +21,10 @@ ADL_SPACE_AREA_NAME="Space-Area-v1.json"
 ADL_SPACE_AREA_MODEL="dtmi:com:motojin:Space:Area;1"
 ADL_SENSOR_THERMOSTAT_NAME="Sensor-Thermostat-v1.json"
 ADL_SENSOR_THERMOSTAT_MODEL="dtmi:com:motojin:Sensor:Thermostat;1"
+ADT_ENDPOINT_IOTHUB2ADT_ALL="endpoint-wedx-adt-dtroute"
+ADT_ENDPOINT_ADT2MAPS_UPD="endpoint-wedx-adt-dtupdate"
+ADT_ROUTE_IOTHUB2ADT_ALL="route-wedx-adt-dtroute"
+ADT_ROUTE_ADT2MAPS_UPD="route-wedx-adt-dtupdate"
 
 # check if we need to log in
 # if we are executing in the Azure Cloud Shell, we should already be logged in
@@ -40,7 +44,7 @@ If you want to change to a different subscription, enter the name or id.
 Or just press enter to continue with the current subscription."
 read -p ">> " SUBSCRIPTION_ID
 if ! test -z "$SUBSCRIPTION_ID"
-then 
+then
     az account set -s "$SUBSCRIPTION_ID"
     echo -e "Now using:"
     az account show --query '[name,id]'
@@ -66,6 +70,14 @@ if [ ! -f "$ADL_SENSOR_THERMOSTAT_NAME" ]; then
     exit 1
 fi
 
+# register with the Azure Digital Twins namespace
+az provider register --namespace 'Microsoft.DigitalTwins'
+
+# assign user role
+echo -e "assign user role."
+CLI_USER=$(az account list --query "[?isDefault && state == 'Enabled'] | [0].user.name" -o tsv 2>/dev/null)
+az dt role-assignment create --resource-group $RESOURCE_GROUP --dt-name $ADT_NAME --assignee $CLI_USER --role "Azure Digital Twins Data Owner" -o none
+
 # create ditital twins models
 echo -e "create digital twins floor models."
 if test -z "$(az dt model list --resource-group $RESOURCE_GROUP --dt-name $ADT_NAME --query="[?id=='${ADL_SPACE_FLOOR_MODEL}'].id" -o tsv)"; then
@@ -84,6 +96,19 @@ if test -z "$(az dt model list --resource-group $RESOURCE_GROUP --dt-name $ADT_N
     az dt model create --resource-group $RESOURCE_GROUP --dt-name $ADT_NAME --models $ADL_SENSOR_THERMOSTAT_NAME -o none
 else
     echo -e "skip creating thermostat model becouse it is existed."
+fi
+
+# create digital twins event routes
+echo -e "create digital twins event routes"
+if test -z $(az dt endpoint list --dt-name $ADT_NAME --resource-group $RESOURCE_GROUP --query="[?name=='${ADT_ENDPOINT_IOTHUB2ADT_ALL}'].name" -o tsv); then
+  echo -e "skip creating digital twins event route - IoT Hub to ADT"
+else
+  az dt route create --dt-name $ADT_NAME --resource-group $RESOURCE_GROUP --endpoint-name $ADT_ENDPOINT_IOTHUB2ADT_ALL --route-name $ADT_ROUTE_IOTHUB2ADT_ALL -o none
+fi
+if test -z $(az dt endpoint list --dt-name $ADT_NAME --resource-group $RESOURCE_GROUP --query="[?name=='${ADT_ENDPOINT_ADT2MAPS_UPD}'].name" -o tsv); then
+  echo -e "skip creating digital twins event route - ADT to Indoor Maps"
+else
+  az dt route create --dt-name $ADT_NAME --resource-group $RESOURCE_GROUP --endpoint-name $ADT_ENDPOINT_ADT2MAPS_UPD --route-name $ADT_ROUTE_ADT2MAPS_UPD --filter "type = 'Microsoft.DigitalTwins.Twin.Update'" -o none
 fi
 
 # list digital twins models
